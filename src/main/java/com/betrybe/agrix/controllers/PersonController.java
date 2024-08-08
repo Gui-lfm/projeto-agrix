c016131d92d9c05ecf779c5ba450b63c0232d207
Guilherme Lucena <guilherme.lucena17@gmail.com>
2024-08-08 13:51:32 -0300

    docs(PersonController): adicionada documentação do enpoint POST /person ao swagger

--- src/main/java/com/betrybe/agrix/controllers/PersonController.java
+++ src/main/java/com/betrybe/agrix/controllers/PersonController.java
@@ -4,6 +4,11 @@ import com.betrybe.agrix.controllers.dtos.PersonDto;
 import com.betrybe.agrix.controllers.dtos.PersonResponseDto;
 import com.betrybe.agrix.models.entities.Person;
 import com.betrybe.agrix.services.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Camada controller de Person.
  */
-@Tag(name = "Person", description = "Adiciona uma pessoa usuária ao banco de dados.")
+@Tag(name = "Person",
+    description = "Endpoint responsável por gerenciar usuários do banco de dados.")
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
@@ -31,6 +37,20 @@ public class PersonController {
   /**
    * Endpoint responsável por adicionar uma pessoa ao banco de dados.
    */
+  @Operation(
+      summary = "Adiciona uma pessoa usuária ao banco de dados.",
+      description = "Retorna um objeto de usuário contendo o id, username e role"
+  )
+  @ApiResponse(
+      responseCode = "201",
+      content = {
+          @Content(
+              schema = @Schema(implementation = PersonResponseDto.class),
+              mediaType = "application/json"
+          )
+      }
+      )
+
   @PostMapping
   public ResponseEntity<PersonResponseDto> postPerson(@RequestBody PersonDto personDto) {
     Person person = personService.create(personDto.toEntity());
